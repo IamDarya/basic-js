9810b1c95964b6299227668673fd980fabebb29f
IamDarya <daria.vyacheslavovna@gmail.com>
2021-03-08 21:06:00 +0100

    fix: fix few tasks
    "Count cats", "Dream team", "What season"

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,19 @@
+// const CustomError = require("../extensions/custom-error");
+
+// module.exports = function countCats( matrix ) {
+//   throw new CustomError('Not implemented');
+//   // remove line with error and write your code here
+// };
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats( /* matrix */ array) {
+    let flatArr = array.flat();
+    let calc = 0;
+    for (let i = 0; i < flatArr.length; i++) {
+        if (flatArr[i] === '^^') {
+            calc++;
+        }
+    }
+
+    return calc;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,29 @@
+// const CustomError = require("../extensions/custom-error");
+
+// module.exports = function createDreamTeam( members ) {
+//   throw new CustomError('Not implemented');
+//   // remove line with error and write your code here
+// };
+
+
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+    let newArr = [];
+    let ifNoString = 0;
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            if (typeof arr[i] === 'string') {
+                newArr.push(arr[i].trim().toUpperCase().slice(0, 1));
+                newArr.sort();
+                ifNoString++;
+            }
+        }
+        if (ifNoString === 0) {
+            return false;
+        }
+    } else {
+        return false;
+    }
+    return newArr.join('');
 };
