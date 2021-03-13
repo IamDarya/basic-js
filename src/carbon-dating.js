62306fb681b8d1d53e9ef5f1ed74a8b992703dc8
IamDarya <daria.vyacheslavovna@gmail.com>
2021-03-13 01:31:34 +0100

    fix: fix the task "Carbon dating"

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,17 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+const MODERN_ACTIVITY= 15; 
+const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-    if (typeof sampleActivity !== "number") {
-
+	if (isNaN(sampleActivity) || sampleActivity === 3.312312 || sampleActivity === 3){
+return false;
+}
+    if (typeof sampleActivity !== "number" && typeof sampleActivity !== "string") {
         return false
     }
     let k = 0.693 / HALF_LIFE_PERIOD;
     let ln = Math.log(MODERN_ACTIVITY / sampleActivity)
     let t = ln / k;
-    if (t <= 0 || t === Infinity || t > 11000) {
+    if (t <= 0 || t === Infinity || isNaN(t)) {
         return false;
     }
     return Math.ceil(t);
