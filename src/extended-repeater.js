ec75ea0b81fe36da1ac813d0d3b019fdaff83372
IamDarya <daria.vyacheslavovna@gmail.com>
2021-03-19 12:43:33 +0100

    fix: fix the task "extended-repeater"

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let arr = [];
+    if (options.additionRepeatTimes === undefined) {
+        options.additionRepeatTimes = 1;
+    }
+    if (options.repeatTimes === undefined) {
+        options.repeatTimes = 1;
+    }
+    if (options.separator === undefined) {
+        options.separator = '+';
+    }
+    if (options.additionSeparator === undefined) {
+        options.additionSeparator = '|';
+    }
+    if (options.addition === null) {
+        options.addition = 'null';
+    }
+    if (str === null) {
+        str = 'null';
+    }
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+        arr.push(options.addition);
+    }
+    arr = arr.join(`${options.additionSeparator}`);
+    let arrN = [];
+    arrN.push(arr);
+    arrN.unshift(str);
+    arrN = arrN.join('');
+    let arrL = [];
+    for (let i = 0; i < options.repeatTimes; i++) {
+        arrL.push(arrN);
+    }
+    arrL = arrL.join(`${options.separator}`)
+    return arrL;
 };
   
